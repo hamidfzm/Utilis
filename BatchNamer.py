cc407726c31e37815193485e7ad6d2245179b322
Hamid FzM <hamidfzm@gmail.com>
2014-10-27 17:12:00 +0330

    improving bachnamer for getting multiple extensions

--- BatchNamer.py
+++ BatchNamer.py
@@ -16,13 +16,18 @@ img002.jpg, etc., img100.jpg with this utility. Use asterisk wildcard \
 for file name pattern.\n\
 For Example: img*.jpg\n")
 
+def dotextention(extension):
+    if extension.startswith('.'):
+        return extension
+    return '.%s'%extension
+
 directory = raw_input('Enter folder [default](%s): '%getcwd()) or getcwd()
 pattern = raw_input('Enter files naming pattern [default](*): ') or '*'
-extension = raw_input('Enter files naming filter [default](*): ')
+extension = raw_input('Enter files naming filter [default](*): ').split(',')
 
 if extension is not None:
-    extension = '.%s'%extension
-wildcard = int(raw_input('Enter wildcard size [default](2): ') or 2) 
+    extension = tuple(map(dotextention, extension))
+wildcard = int(raw_input('Enter wildcard size [default](2): ') or 2)
 
 counter = 0
 
@@ -32,7 +37,7 @@ for item in listdir(directory):
     if item.endswith(extension):
         counter += 1
 
-        dest = pattern.replace('*', str(counter).zfill(wildcard))
+        dest = pattern.replace('*', str(counter).zfill(wildcard)) + path.splitext(item)[1]
         rename(path.join(directory, item),
                path.join(directory, dest))
         print('%s renamed to %s'%(item, dest))
